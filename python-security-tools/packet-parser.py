957fb6b6d78459e6c6ea95458f0296b552d2a05f
Alfonso-security <kranegameit@gmail.com>
2026-05-19 22:16:15 +0200

    Create packet-parser.py

---
+++ python-security-tools/packet-parser.py
@@ -0,0 +1,32 @@
+import socket
+import struct
+
+sniffer = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+
+print("Listening for packets...\n")
+
+while True:
+
+    raw_data, addr = sniffer.recvfrom(65535)
+
+    ethernet_header = raw_data[0:14]
+
+    eth = struct.unpack("!6s6sH", ethernet_header)
+
+    protocol = socket.htons(eth[2])
+
+    print(f"\n[ETHERNET FRAME]")
+    print(f"Protocol: {protocol}")
+
+    if protocol == 8:
+
+        ip_header = raw_data[14:34]
+
+        iph = struct.unpack("!BBHHHBBH4s4s", ip_header)
+
+        source_ip = socket.inet_ntoa(iph[8])
+
+        destination_ip = socket.inet_ntoa(iph[9])
+
+        print(f"Source IP: {source_ip}")
+        print(f"Destination IP: {destination_ip}")
